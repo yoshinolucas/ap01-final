5dedc0e757d5b7baf1b0615b99beba8101f9c868
Lucas Yudi Yoshino <2202601@SALAS.AULAS>
2024-03-18 21:22:04 -0300

    fix validator

--- app/schemas/pagamento.py
+++ app/schemas/pagamento.py
@@ -4,7 +4,7 @@ import re
 
 class Pagamento(BaseModel):
     id:int
-    metodo_pagamento: int
+    metodo_pagamento: str
     valor: float
     cnpj_comprador: str
     id_usuario: int
@@ -13,6 +13,12 @@ class Pagamento(BaseModel):
     def validate_cnpj_comprador(cls, value):
         if not re.match(r"\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}", value):
             raise ValueError('Invalid CNPJ')
+        return value
+    
+    @validator('metodo_pagamento')
+    def validate_metodo(cls,value):
+        if value != 'Cartão' or value != 'Dinheiro':
+            raise ValueError('Método Inválido')
         return value
 
 
